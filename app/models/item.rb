16f20f0132d9933d4194ca8c97ba9e054f2a7519
caward12 <ward.colleen.a@gmail.com>
2017-05-04 16:31:28 -0600

    add items ranked by most sold

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,12 @@ class Item < ApplicationRecord
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
+  def self.top_sold_quantity(limit)
+    select('items.*, count(invoice_items.id) AS item_count').
+    joins(invoice_items: [{invoice: :transactions}]).
+    group('items.id').
+    order('item_count DESC').
+    limit(limit)
+  end
 
-  # scope :ordered_by_created_at, -> { order(created_at: :asc)}
 end
