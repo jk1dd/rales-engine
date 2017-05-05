c7e9d65fbc06420468a91c8f1a1fc7e90787951f
caward12 <ward.colleen.a@gmail.com>
2017-05-04 23:53:05 -0600

    bolstering unit tests

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,15 @@
-# require 'rails_helper'
-#
-# RSpec.describe Invoice, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Invoice, type: :model do
+  context 'attributes' do
+    it 'has status, merchant_id, customer_id, created_at, updated_at' do
+      i = create(:invoice)
+
+      expect(i).to respond_to(:status)
+      expect(i).to respond_to(:customer_id)
+      expect(i).to respond_to(:merchant_id)
+      expect(i).to respond_to(:created_at)
+      expect(i).to respond_to(:updated_at)
+    end
+  end
+end
